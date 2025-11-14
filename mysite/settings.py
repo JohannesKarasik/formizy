fc24fdb733678a43b1c12f615150ba9a632d8b4d
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-14 23:29:13 +0100

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,11 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
+
+
 
 
 # Default primary key field type
