04e0936066a897134be7d3beca6ea66a6cd63bc6
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-15 04:41:37 +0100

    Initial commit

--- main/models.py
+++ main/models.py
@@ -62,4 +62,15 @@ class PaidForm(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.form_slug}"
+    
+
+class GeneratedPDF(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    form_slug = models.CharField(max_length=200)
+    pdf_file = models.FileField(upload_to="generated_pdfs/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.form_slug}"
+
 
