3d71847a62ca8b62b697771cae4f7effb9ee32e5
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-17 22:14:26 +0100

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -17,7 +17,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/clinton/formizy/media'
+
 
 
 
