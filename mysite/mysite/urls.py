868d3b784815ef7a810400a9034ee1c6e759c56a
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-19 02:17:45 +0100

    Initial commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,12 @@
+# mysite/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import stripe_webhook
 
+# ➜ ADD THESE
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import FormSitemap
 
@@ -12,26 +15,13 @@ sitemaps = {
 }
 
 urlpatterns = [
-
-    # Language switch endpoint (required)
-    path('i18n/', include('django.conf.urls.i18n')),
-
-    # Admin & webhook outside translations
     path('admin/', admin.site.urls),
-    path("webhook/stripe/", include('main.urls')),
-
-    # MAIN ROUTES WITHOUT LANG PREFIX
-    # <-- This keeps: /  /de  /es /dk  etc. working
+    path("webhook/stripe/", stripe_webhook, name="stripe_webhook"),
     path('', include('main.urls')),
+
+    # ➜ ADD THIS LINE
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
 ]
 
-# TRANSLATED VERSION OF ROUTES UNDER /lang/
-# This does NOT replace your existing routes
-urlpatterns += i18n_patterns(
-    path('lang/', include('main.urls')),
-    prefix_default_language=False,
-)
-
-# Static (dev mode)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
