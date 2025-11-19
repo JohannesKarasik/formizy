8a41c72ac43bce2e236b2e1c83c9a007de0b15ea
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-19 02:14:28 +0100

    Initial commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,35 +4,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import stripe_webhook
 
-# Sitemap imports
+# ➜ ADD THESE
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import FormSitemap
-
-# ➜ Required for Django i18n
-from django.conf.urls.i18n import i18n_patterns
 
 sitemaps = {
     'forms': FormSitemap,
 }
 
 urlpatterns = [
-    # Language switcher endpoint (required)
-    path('i18n/', include('django.conf.urls.i18n')),
-
-    # Admin stays OUTSIDE language patterns
     path('admin/', admin.site.urls),
+    path("webhook/stripe/", stripe_webhook, name="stripe_webhook"),
+    path('', include('main.urls')),
 
-    # Stripe webhook also stays OUTSIDE
-    path("webhook/stripe/", include('main.urls')),
+    # ➜ ADD THIS LINE
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
 ]
 
-# ➜ Wrap your main URLs in i18n_patterns
-urlpatterns += i18n_patterns(
-    path("", include("main.urls")),
-    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
-)
-
-# Static files (dev only)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
