140cba4d1cd052ea25a83db7efa2f2d594881e2e
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2025-11-19 02:15:21 +0100

    Initial commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,9 @@
-# mysite/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import stripe_webhook
 
-# ➜ ADD THESE
 from django.contrib.sitemaps.views import sitemap
 from main.sitemaps import FormSitemap
 
@@ -15,13 +12,20 @@ sitemaps = {
 }
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("webhook/stripe/", stripe_webhook, name="stripe_webhook"),
-    path('', include('main.urls')),
+    # Language switcher endpoint
+    path('i18n/', include('django.conf.urls.i18n')),
 
-    # ➜ ADD THIS LINE
-    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
+    # Admin and webhooks must NOT be translated
+    path('admin/', admin.site.urls),
+    path("webhook/stripe/", include('main.urls')),
 ]
 
+# IMPORTANT: use a custom prefix to avoid conflicts with your country codes.
+urlpatterns += i18n_patterns(
+    path('lang/', include('main.urls')),  # ← LANG CODE prefix
+    prefix_default_language=False,
+)
+
+# Static files (development)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
